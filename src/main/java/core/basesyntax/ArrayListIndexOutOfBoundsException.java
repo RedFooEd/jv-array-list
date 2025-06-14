bb5c3f302d571a9bb562f1fa08f6acf546de2e3e
Denis Burlaka <burlakadenis65@gmail.com>
2025-06-14 16:39:56 +0300

    Fixed Exception

--- src/main/java/core/basesyntax/ArrayListIndexOutOfBoundsException.java
+++ src/main/java/core/basesyntax/ArrayListIndexOutOfBoundsException.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
 public class ArrayListIndexOutOfBoundsException extends RuntimeException {
+    
+    public ArrayListIndexOutOfBoundsException() {
+        super("Index out of bounds");
+    }
+    
     public ArrayListIndexOutOfBoundsException(String message) {
-        super(message);
+        super("ArrayList index error: " + message);
     }
 }
